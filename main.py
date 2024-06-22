c475a0a39893f7ddaef9f107196cd18873132da4
Trimpta <68428753+trimpta@users.noreply.github.com>
2024-06-22 11:28:50 +0530

    done?

---
+++ main.py
@@ -0,0 +1,40 @@
+import re
+from answer_key import *
+
+with open("response.txt") as rizz:
+    brainrot = rizz.readlines()[11:]
+    gyatt = []
+    for i in brainrot:
+        if i[0] != "Q":
+
+            gyatt.append(i.lstrip())
+
+    
+
+    gyatt =  "".join(gyatt)
+
+
+    ohio = []
+
+    for i in re.findall(r'[ABCDE]|NA', gyatt):
+        ohio.append(i.lstrip())
+
+
+sc = 0
+
+sigmarizz = input("Exam on (5/6/7/8/9) :")
+skibidi = int(input("Enter Physics + Chemistry + Maths mark out of 300 :"))
+
+for i, j in zip(ohio, keys[sigmarizz]):
+
+    if i == "NA":
+        continue 
+    
+    if i == j:
+        sc += 4
+    else:
+        sc -=1
+
+print(f"{150 - len(keys[sigmarizz])} Questions Cancelled.")
+print(f"{sc} marks out of {len(keys[sigmarizz])*4} in keam")
+print(f"total marks: {(sc + skibidi)/2} out of {(300 + len(keys[sigmarizz])*2)}")
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Keam Mark caallcuatro
+
+# Go to https://www.cee.kerala.gov.in/keam2024/ , And click on Candidate response in the left panel
+# Press Ctrl + C, and paste it in response.txt.
+
+# Run main.py, and enter your exam date and board exam marks.
